34a99fbd70c4b5ac7840ba17ab578a6a3daae8ab
sasha <elshurimem1337@gmail.com>
2022-11-13 21:16:55 +0300

    2131231

--- lab4/Source2.cpp
+++ lab4/Source2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <list>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 class fileMethods {
@@ -49,13 +50,102 @@ public:
 		cout << 100 / double(count) * upCount << " " <<  100 - (100 / double(count) * upCount) << endl;
 		cout << count << ' ' << upCount << ' ' << lowCount << endl;
 	}
+	void symbol_line() {
+		string tmp;
+		char sym;
+		cin >> sym;
+		int count, symcount, num = 1;
+		while (!inputfile.eof()) {
+			inputfile >> tmp;
+			count = 0;
+			symcount = 0;
+			for (int i = 0; i < tmp.length(); ++i) {
+				count++;
+				if (tmp[i] == sym) symcount++;
+			}
+			double percent;
+			cout << "¬ строке є" << num << ": " << 100 / double(count) * symcount << "%" << endl;
+			num++;
+		}
+	}
+	void symbol_text() {
+		string tmp;
+		char sym;
+		cin >> sym;
+		int count = 0, symcount = 0;
+		while (!inputfile.eof()) {
+			inputfile >> tmp;
+			for (int i = 0; i < tmp.length(); ++i) {
+				count++;
+				if (tmp[i] == sym) symcount++;
+			}
+		}
+		double percent;
+		cout << "¬еро€тность по€влени€ символа в тексте: " << 100 / double(count) * symcount << "%" << endl;
+	}
+	void sort() {
+		vector <string> vec;
+		string tmp;
+		int alphabet, pos; // 1 - русский, 2 - английский
+		char sym;
+		bool flag; //
+		getline(inputfile, tmp);
+		vec.insert(vec.begin(), tmp);
+		if (tmp[0] < 0) alphabet = 1;
+		else alphabet = 2;
+		switch (alphabet) { // если символ заглавный +32 
+			case 1: 
+				while (getline(inputfile, tmp)) {
+					flag = true; // нужен чтобы вставить строку в конец вектора
+					pos = 0;
+					sym = tmp[0];
+					if (sym <= -33) sym += 32;
+					for (auto i = vec.begin(); i < vec.end(); ++i) {
+						char symtmp = i[0][0];
+						if (symtmp <= -33) symtmp += 32;
+						if (sym <= symtmp) {
+							advance(i, pos);
+							vec.insert(i, tmp);
+							flag = false;
+							break;
+						}
+					}
+					if (flag) {
+						vec.insert(vec.end(), tmp);
+					}
+				}
+				break;
+			case 2:
+				while (getline(inputfile, tmp)) {
+					flag = true; // нужен чтобы вставить строку в конец вектора
+					pos = 0;
+					sym = tmp[0];
+					if (sym <= 90) sym += 32;
+					for (auto i = vec.begin(); i < vec.end(); ++i) {
+						char symtmp = i[0][0];
+						if (symtmp <= 90) symtmp += 32;
+						if (sym <= symtmp) {
+							advance(i, pos);
+							vec.insert(i, tmp);
+							flag = false;
+							break;
+						}
+					}
+					if (flag) {
+						vec.insert(vec.end(), tmp);
+					}
+				}
+				break;
+		}
+	}
 private:
 	ifstream inputfile;
 };
 
 int main() {
+	setlocale(LC_ALL, "RU");
 	fileMethods a("write1.txt");
-	a.upper_lower();
+	a.sort();
 	string abc = "/521.,";
 	return 1;
 }
